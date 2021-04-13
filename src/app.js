b0924e5fadac770370efbd843c1a9ea2b97840c2
EresDev <eresdev@hotmail.com>
2021-04-13 08:09:26 -0700

    feat: use template engine for pages
    the pages covered here are: home, about, help.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,34 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
+
+const staticContentPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+app.set('views', viewsPath);
+app.set('view engine', 'hbs');
+app.use(express.static(staticContentPath));
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Eresdev'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Eresdev'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send('Weather');
