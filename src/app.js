e8393ac181de2a9f986adcd07026f45218685c1f
EresDev <eresdev@hotmail.com>
2021-04-13 09:19:19 -0700

    feat: reuse partials in template
    the pages covered here are: home, about, help.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
-
 const staticContentPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 app.set('views', viewsPath);
 app.set('view engine', 'hbs');
 app.use(express.static(staticContentPath));
-
+hbs.registerPartials(partialsPath);
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -21,13 +22,15 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('index', {
-        title: 'Weather app',
+        title: 'About',
         name: 'Eresdev'
     });
 });
 
 app.get('/help', (req, res) => {
     res.render('help', {
+        title: 'Help',
+        name: 'Eresdev',
         helpText: 'This is some helpful text.'
     });
 });
