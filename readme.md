d46b512df0d454d611be6e2c74b0a550196faf61
EresDev <eresdev@hotmail.com>
2021-04-15 07:34:14 -0700

    feat: include forecast and geocode api consumers
    with their dependencies require, dotenv

---
+++ readme.md
@@ -0,0 +1,9 @@
+
+
+This application consumes two APIs.
+1. [openweathermap.org](https://openweathermap.org/current)  API provides conversion of address to latitude and longitude.
+2. [mapbox.com](https://docs.mapbox.com/api/search/geocoding) API provides weather forecast for given latitude longitude.
+
+You can sign up for a free account with limited request numbers to try this application.
+
+Once you have the API keys for both, rename the .env-sample to .env file and add these API keys there.
---
+++ src/api-consumers/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const apiKey = () => process.env.OPENWEATHERMAP_API_KEY ;
+//API  Docs: https://openweathermap.org/current
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lat='+latitude+'&lon='+longitude+'&appid='+apiKey();
+
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to the weather API.', undefined);
+        } else if(body.cod == 400 ) {
+            callback('Unable to find location for latlong.', undefined);
+        }else {
+            const tempC = body.main.temp;
+            callback(undefined, 'It is currently '+tempC+' degree out.');
+        }
+    });
+}
+
+module.exports = forecast;
