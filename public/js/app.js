278a1219d47aa7018db395adf48291813380e819
EresDev <eresdev@hotmail.com>
2021-04-16 20:40:39 +0500

    feat(frontend): fetch the weather from the API
    Default address Boston

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
-console.log('This is frontend JS file.');
+(function(){
+    fetch('/weather?address=Boston').then((response) => {
+        response.json().then((jsonResponse) => {
+            if (jsonResponse.error) {
+                console.error(jsonResponse);
+            } else {
+                console.log(jsonResponse);
+            }
+        });
+    });
+})();
