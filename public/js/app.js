abd3ebdab5b9a5855304262cc90ee5c5fd2102b6
EresDev <eresdev@hotmail.com>
2021-04-19 20:21:20 +0500

    feat: display weather api result or error
    on html, instead of console.

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,27 @@
 (function(){
     const form = document.querySelector('form');
     const input = document.querySelector('input');
+    const messageOne = document.querySelector('#message-1');
+    const messageTwo = document.querySelector('#message-2');
+
+    messageOne.textContent = '';
+    messageTwo.textContent = '';
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
+
+        messageOne.textContent = 'Loading...';
+        messageTwo.textContent = '';
 
         const location = input.value;
 
         fetch('/weather?address=' + location).then((response) => {
             response.json().then((jsonResponse) => {
                 if (jsonResponse.error) {
-                    console.error(jsonResponse);
+                    messageOne.textContent = jsonResponse.error;
                 } else {
-                    console.log(jsonResponse);
+                    messageOne.textContent =  jsonResponse.location;
+                    messageTwo.textContent =  jsonResponse.forecast;
                 }
             });
         });
