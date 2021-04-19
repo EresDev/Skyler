ce9d5e9c757179c4ab232085c1a7fe3a6fe4e990
EresDev <eresdev@hotmail.com>
2021-04-19 19:53:10 +0500

    feat: make search form functional
    add javascript to call weather api

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,22 @@
 (function(){
-    fetch('/weather?address=Boston').then((response) => {
-        response.json().then((jsonResponse) => {
-            if (jsonResponse.error) {
-                console.error(jsonResponse);
-            } else {
-                console.log(jsonResponse);
-            }
+    const form = document.querySelector('form');
+    const input = document.querySelector('input');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const location = input.value;
+
+        fetch('/weather?address=' + location).then((response) => {
+            response.json().then((jsonResponse) => {
+                if (jsonResponse.error) {
+                    console.error(jsonResponse);
+                } else {
+                    console.log(jsonResponse);
+                }
+            });
         });
+
     });
+
 })();
